f47a0397a6692b88846c7cf5cdf8397388ef8f70
Jhonatan Prieto <jhonatan0913@gmail.com>
2024-10-09 16:33:20 -0500

    Se crea la clase Car y el formulario

---
+++ EjemploFundamentosCSharp/Proyecto2/Car.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Proyecto2
+{
+    public class Car
+    {
+        // Atributos
+        string color;
+        int cilindraje;
+        int modelo;
+
+        // Metodo
+        public string stop()
+        {
+            string msj = "El carro esta detenido";
+            return msj;
+        }
+
+    }
+}
